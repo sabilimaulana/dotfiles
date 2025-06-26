7b47cdd10b431a98c69dde091cceaaa1fbd0fd1a
Sabili Maulana <spacysbl.learn@gmail.com>
2025-06-26 16:12:16 +0700

    feat: better dont use desktop from nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,14 +19,18 @@
   # environment.
   home.packages = [
     pkgs.bun
+    pkgs.gh
     pkgs.git
-    
+    pkgs.nodejs_24
+
     pkgs.discord
     pkgs.bitwarden-desktop
-    pkgs.zed-editor
-    pkgs.ghostty 
-    
-    inputs.zen-browser.packages.x86_64-linux.generic
+    #pkgs.zed-editor
+    #pkgs.ghostty
+
+    #inputs.zen-browser.packages.x86_64-linux.generic
+
+    # sudo pacman -S zed zen-browser ghostty
 
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
