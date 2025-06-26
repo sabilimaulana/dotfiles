ab12f65ca5e405fb60de7babbd501ac185e6632d
Sabili Maulana <spacysbl.learn@gmail.com>
2025-06-26 14:18:13 +0700

    feat: fish

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set -gx XDG_DATA_DIRS $HOME/.nix-profile/share $HOME/.local/share /usr/share/applications  $XDG_DATA_DIRS
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
