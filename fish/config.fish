3cf03062a18d0d75de80012e25a06873c5c83ef6
Sabili Maulana <spacysbl.learn@gmail.com>
2025-06-26 14:21:18 +0700

    fix: fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-set -gx XDG_DATA_DIRS $HOME/.nix-profile/share $HOME/.local/share /usr/share/applications  $XDG_DATA_DIRS
+#set -gx XDG_DATA_DIRS $HOME/.nix-profile/share $HOME/.local/share /usr/share/applications  $XDG_DATA_DIRS
 
 # overwrite greeting
 # potentially disabling fastfetch
